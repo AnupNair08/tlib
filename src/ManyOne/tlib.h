b255444ca1806f0bbb44f09db2910fc74613d06c
Hrishikesh Athalye <hathalye7@gmail.com>
2021-04-12 14:38:02 +0530

    chore: Changed free list handling

--- src/ManyOne/tlib.h
+++ src/ManyOne/tlib.h
@@ -1,19 +1,27 @@
-#include<stdlib.h>
-#include<unistd.h>
-#include<setjmp.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <setjmp.h>
 #include "locks.h"
 
 typedef unsigned long thread;
-typedef struct funcargs{
+typedef struct funcargs
+{
     void (*f)(void *);
-    void* arg;
+    void *arg;
 } funcargs;
 
 int createManyOne(thread *, void *, void *, void *);
 
-enum thread_state {RUNNING, WAITING, EXITED, RUNNABLE};
+enum thread_state
+{
+    RUNNING,
+    WAITING,
+    EXITED,
+    RUNNABLE
+};
 
-typedef struct tcb {
+typedef struct tcb
+{
     thread tid;
     void *stack;
     size_t stack_sz;
@@ -22,21 +30,23 @@ typedef struct tcb {
     //indicate if the process has exited or not
     int exited;
     //list of all waiters on this process
-    int* waiters;
+    int *waiters;
     int numWaiters;
-    mut_t* mutexWait;
+    mut_t *mutexWait;
     // Implement as a queue for easy deletion
-    int* pendingSig;
+    int *pendingSig;
     int numPendingSig;
-    funcargs* args;
+    funcargs *args;
 } tcb;
 
-typedef struct qnode {
+typedef struct qnode
+{
     tcb *tcbnode;
     struct qnode *next;
 } qnode;
 
-typedef struct tcbQueue {
+typedef struct tcbQueue
+{
     qnode *front;
     qnode *back;
     int len;
@@ -45,11 +55,11 @@ typedef struct tcbQueue {
 #define STACK_SZ 65536
 #define GUARD_SZ getpagesize()
 #define TGKILL 234
-#define CLONE_FLAGS CLONE_VM|CLONE_FS|CLONE_FILES|CLONE_SIGHAND|CLONE_THREAD |CLONE_SYSVSEM|CLONE_PARENT_SETTID|CLONE_CHILD_CLEARTID
+#define CLONE_FLAGS CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD | CLONE_SYSVSEM | CLONE_PARENT_SETTID | CLONE_CHILD_CLEARTID
 #define SCHED_INTERVAL 2000
 
 void wrapRoutine();
-static void* allocStack(size_t , size_t );
+static void *allocStack(size_t, size_t);
 static void starttimer();
 void enabletimer();
 void disabletimer();
@@ -58,11 +68,12 @@ static void scheduler();
 static void initManyOne();
 
 int addThread(tcbQueue *, tcb *);
-tcb* getNextThread(tcbQueue *);
-tcb* getThread(tcbQueue *, thread);
+tcb *getNextThread(tcbQueue *);
+tcb *getThread(tcbQueue *, thread);
 void printAllmo(tcbQueue *);
 void queueRunning(tcbQueue *);
+void removeExitedThreads(tcbQueue *);
 int removeThread(tcbQueue *, unsigned long int);
 void reQueue(tcbQueue *, tcb *);
-void unlockMutex(tcbQueue *, mut_t*);
-void initTcb(tcb *, int, thread,sigjmp_buf *);
+void unlockMutex(tcbQueue *, mut_t *);
+void initTcb(tcb *, int, thread, sigjmp_buf *);
